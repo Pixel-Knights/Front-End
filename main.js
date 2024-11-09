95bea8f32774d51caa71bdcbdbbf0a2e428283c0
FaaF-Eng <fabio.teixeira18@hotmail.com>
2024-11-09 15:41:48 -0300

    JavaScript update

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 function enviarMensagem(){ //Função para enviar a mensagem, por enquanto vai apenas jogar pra dentro de uma variável
-    console.log('Teste do Botão')
+    console.log('Teste do Botão');
+    let mensagem = document.getElementsById('mensager').value;
+    console.log(mensagem);
 }
