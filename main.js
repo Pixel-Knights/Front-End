0b479dac9a94ffc190f7352923c891924d7eb214
FaaF-Eng <fabio.teixeira18@hotmail.com>
2024-11-11 11:00:03 -0300

    button upgrade

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 function enviarMensagem(){ //Função para enviar a mensagem, por enquanto vai apenas jogar pra dentro de uma variável
-    console.log('Teste do Botão');
-    let mensagem = document.getElementsById('mensager').value;
+    let mensagem = document.getElementById("mensager").value;
     console.log(mensagem);
 }
